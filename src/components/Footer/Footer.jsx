07f876c119c84961c3dca3cbbcf7a7b52115e419
BakaNoCrimson <Sidney.nguyen@live.fr>
2024-08-14 13:08:49 +0200

    :sparkles:
    optimisation CSS Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,61 +1,64 @@
 import React from 'react';
 import './Footer.css';
+import fioriture from '../../Assets/Icons/fioriture.png';
 import homeIcon from '../../Assets/Icons/home.svg';
 import askIcon from '../../Assets/Icons/ask.svg';
 import caseIcon from '../../Assets/Icons/case.svg';
-import phoneIcon from '../../Assets/Icons/phone.svg';
 import universeIcon from '../../Assets/Icons/universe.svg';
+import mailIcon from '../../Assets/Icons/mail.svg';
 import ArtisanLogo from '../../Assets/photos/ArtisanLogo.png';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-section">
-        <div className="atelier-info">
-          <h4 className="atelier-name">L’ATELIER PERRIER</h4>
-          <p className="atelier-address">Adresse: BLABLABLA</p>
+    <>
+      <div className="fioriture-container">
+        <img src={fioriture} alt="Fioriture" className="fioriture" />
+      </div>
+      <div className="footer">
+        <div className="footer-section atelier-info">
+          <div className="atelier-name">L’ATELIER PERRIER</div>
+          <div className="atelier-address">ADRESSE: BLABLABLA</div>
+        </div>
+        <div className="footer-section">
+          <div className="section-title">accès rapide</div>
+          <div className="quick-links">
+            <div className="nav-item">
+              <img src={homeIcon} alt="Home" className="nav-icon" />
+              <span>Accueil</span>
+            </div>
+            <div className="nav-item">
+              <img src={askIcon} alt="Qui suis-je" className="nav-icon" />
+              <span>Qui suis-je</span>
+            </div>
+            <div className="nav-item">
+              <img src={caseIcon} alt="Mon métier" className="nav-icon" />
+              <span>Mon métier</span>
+            </div>
+            <div className="nav-item">
+              <img src={universeIcon} alt="Mon Univers" className="nav-icon" />
+              <span>Mon Univers</span>
+            </div>
+            <div className="nav-item">
+              <img src={mailIcon} alt="Me contacter" className="nav-icon" />
+              <span>Me contacter</span>
+            </div>
+          </div>
+        </div>
+        <div className="footer-section">
+          <div className="section-title">horaires de travail</div>
+          <div className="quick-links">
+            <div className="nav-item">Lundi: 09h-18h</div>
+            <div className="nav-item">Mardi: 09h-18h</div>
+            <div className="nav-item">Mercredi: 09h-18h</div>
+            <div className="nav-item">Jeudi: 09h-18h</div>
+            <div className="nav-item">Vendredi: 09h-18h</div>
+          </div>
+        </div>
+        <div className="footer-section">
+          <img src={ArtisanLogo} alt="Artisan Logo" className="artisan-logo" />
         </div>
       </div>
-
-      <div className="footer-section">
-        <h2 className="section-title">Accès rapide</h2>
-        <div className="quick-links">
-          <div className="nav-item">
-            <img className="nav-icon" src={homeIcon} alt="Accueil" />
-            <span>Accueil</span>
-          </div>
-          <div className="nav-item">
-            <img className="nav-icon" src={askIcon} alt="Qui suis-je" />
-            <span>Qui suis-je</span>
-          </div>
-          <div className="nav-item">
-            <img className="nav-icon" src={caseIcon} alt="Mon métier" />
-            <span>Mon métier</span>
-          </div>
-          <div className="nav-item">
-            <img className="nav-icon" src={universeIcon} alt="Mon Univers" />
-            <span>Mon Univers</span>
-          </div>
-          <div className="nav-item">
-            <img className="nav-icon" src={phoneIcon} alt="Me contacter" />
-            <span>Me contacter</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="footer-section">
-        <h2 className="section-title">Horaires de travail</h2>
-        <p>Lundi: 09h-18h</p>
-        <p>Mardi: 09h-18h</p>
-        <p>Mercredi: 09h-18h</p>
-        <p>Jeudi: 09h-18h</p>
-        <p>Vendredi: 09h-18h</p>
-      </div>
-
-      <div className="footer-section">
-        <img className="artisan-logo" src={ArtisanLogo} alt="Artisan Logo" />
-      </div>
-    </footer>
+    </>
   );
 };
 
